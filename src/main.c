eb912b5a9956fd52f22cd98e69f1363ff4f48864
Brenda <brenda.braga1001@gmail.com>
2017-10-01 16:22:06 -0300

    ok

--- src/main.c
+++ src/main.c
@@ -2,12 +2,35 @@
 
 int main() {
   int a[5];
-  int n;
+  int p[5];
+  int n, j=0;
 
   scanf("%d %d %d %d %d %d", &(a[0]), &(a[1]), &(a[2]), &(a[3]), &(a[4]), &n);
-
-  for (int i=0; i<n; i++) {
-    /* Imprime resultado da n-esima iteracao do scheduler */
+        if(1 % a[0] != 0 && 1 % a[1] != 0 && 1 % a[2] != 0 && 1 % a[3] != 0 && 1 % a[4] != 0 ){
+		printf("-");
+	}
+	for(j=0; j<5; j++){
+		if(a[j] == 1){
+			printf("%d", j);	
+		}
+		if(j==4){
+				printf("\n");
+			}
+	}
+	
+  for (int i=2; i<=n; i++) {
+        if(i % a[0] != 0 && i % a[1] != 0 && i % a[2] != 0 && i % a[3] != 0 && i % a[4] != 0 ){
+		printf("-");
+	}
+	for(j=0; j<5; j++){
+    	if(i % a[j] == 0){
+    		printf("%d", j);
+		}
+		if(j==4){
+			  	printf("\n");		
+			}
+	}
+	
   }
 
   return 0;
